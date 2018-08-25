33cdd9e4a62d77f170f24a208d62a706d4cdeedf
shubin.zhang <66003549@163.com>
2018-08-25 11:53:08 +0800

    适配 刘海屏

--- app/src/main/java/com/padtast/notchedscreenadaptation/MainActivity.java
+++ app/src/main/java/com/padtast/notchedscreenadaptation/MainActivity.java
@@ -1,13 +1,22 @@
 package com.padtast.notchedscreenadaptation;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 28) {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(lp);
+        }
         setContentView(R.layout.activity_main);
     }
 }
